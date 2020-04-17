59694ee9e221f2775b4250984bcad2815560db8c
Jonathan Arreguit <jonathan.arreguitoneill@epfl.ch>
2020-04-17 14:32:58 +0200

    [SALAMANDER] Improved default salamander options

--- scripts/salamander.py
+++ scripts/salamander.py
@@ -34,15 +34,16 @@ def main():
             [-1e-1, -1e1, -1e1],
             [-1e-6, -1e-6, -1e-6],
         ],
-        weight_osc_body=1e0,
+        weight_osc_body=1e1,
         weight_osc_legs_internal=3e1,
         weight_osc_legs_opposite=3e0,
         weight_osc_legs_following=3e0,
-        weight_osc_legs2body=1e1,
+        weight_osc_legs2body=3e1,
         weight_sens_contact_i=-2e0,
         weight_sens_contact_e=2e0,
         weight_sens_hydro_freq=-1e-1,
         weight_sens_hydro_amp=-1e-1,
+        body_stand_amplitude=0.4,
     )
 
     (
