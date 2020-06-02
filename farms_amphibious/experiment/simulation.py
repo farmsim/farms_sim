98ce92f225e705b03af1bfc3a6582814b7d042c3
Jonathan Arreguit <jonathan.arreguitoneill@epfl.ch>
2020-06-02 13:58:24 +0200

    [PROFILE] Added option to select filename

--- farms_amphibious/experiment/simulation.py
+++ farms_amphibious/experiment/simulation.py
@@ -112,9 +112,10 @@ def profile(function, **kwargs):
     n_time = kwargs.pop('pstat_n_time', 30)
     n_cumtime = kwargs.pop('pstat_n_cumtime', 30)
     prof = cProfile.Profile()
+    profile_filename = kwargs.pop('profile_filename', 'simulation.profile')
     result = prof.runcall(function, **kwargs)
-    prof.dump_stats('simulation.profile')
-    pstat = pstats.Stats('simulation.profile')
+    prof.dump_stats(profile_filename)
+    pstat = pstats.Stats(profile_filename)
     pstat.sort_stats('time').print_stats(n_time)
     pstat.sort_stats('cumtime').print_stats(n_cumtime)
     return result
