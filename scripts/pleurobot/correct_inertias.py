ac18e0438f2aa857fdd0584163125080159414e9
Jonathan Arreguit <jonathan.arreguitoneill@epfl.ch>
2020-09-11 08:44:22 +0200

    [PLEUROBOT] Added script to correct original SDF files

---
+++ scripts/pleurobot/correct_inertias.py
@@ -0,0 +1,68 @@
+"""Correct inertias"""
+
+import os
+import numpy as np
+from farms_sdf.sdf import ModelSDF, Visual
+from farms_models.utils import get_sdf_path, create_new_model_from_farms_sdf
+
+
+def main():
+    """Main"""
+    filename = get_sdf_path(name='pleurobot', version='0')
+    sdf = ModelSDF.read(filename=filename)[0]
+    directory = os.path.dirname(filename)
+
+    # Correct inertias
+    for link in sdf.links:
+        if link.visuals:
+            visual = link.visuals[0]
+            inertial = link.inertial
+            mesh_path = os.path.join(directory, visual.geometry.uri)
+            link.inertial = inertial.from_mesh(
+                mesh=mesh_path,
+                scale=visual.geometry.scale[0],
+                pose=visual.pose,
+                units=inertial.units,
+                mass=inertial.mass,
+            )
+            # link.inertial.inertias = inertial.inertias
+        else:
+            link.inertial.inertias = [0, 0, 0, 0, 0, 0]
+
+    # Add eyes
+    for link in sdf.links:
+        if link.name == 'Head':
+            eye = Visual.sphere(
+                'eye_left',
+                pose=[0.16, -0.07, 0, 0, 0, 0],
+                units=link.units,
+                radius=0.05,
+                color=[0, 0, 0, 1],
+            )
+            link.visuals.append(eye)
+            eye = Visual.sphere(
+                'eye_right',
+                pose=[0.16, 0.07, 0, 0, 0, 0],
+                units=link.units,
+                radius=0.05,
+                color=[0, 0, 0, 1],
+            )
+            link.visuals.append(eye)
+            break
+
+    # Write
+    filename = create_new_model_from_farms_sdf(
+        name='pleurobot',
+        version='1',
+        sdf=sdf,
+        options={
+            'author': 'Jonathan Arreguit',
+            'email': 'jonathan.arreguitoneill.@epfl.ch',
+            'overwrite': True,
+        }
+    )
+    print(filename)
+
+
+if __name__ == '__main__':
+    main()
