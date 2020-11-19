dbd04713ba3a60adc4d4831822f506c7475cc36a
Jonathan Arreguit <jonathan.arreguitoneill@epfl.ch>
2020-11-19 17:27:20 +0100

    [SALAMANDER] Created folder dedicated to salamander simulations

---
+++ scripts/salamander/salamander.sh
@@ -0,0 +1,11 @@
+# python3.7 salamander.py --duration 10 --timestep 1e-2 --num_sub_steps 0 --n_solver_iters 1000 --residual_threshold 1e-10 --fast
+# python3.7 salamander.py --duration 10 --timestep 1e-2 --num_sub_steps 0 --n_solver_iters 10000 --residual_threshold 1e-1 --fast
+# python3.7 salamander.py --duration 30 --timestep 1e-2 --num_sub_steps 0 --n_solver_iters 1000 --residual_threshold 1e-1 --fast
+
+# Experiment
+# python3.7 salamander.py --duration 30 --timestep 1e-3 --num_sub_steps 0 --n_solver_iters 100 --residual_threshold 1e-1 --fast
+# python3.7 salamander.py --duration 30 --timestep 1e-2 --num_sub_steps 0 --n_solver_iters 10000 --residual_threshold 1e-1 --fast
+# python3.7 salamander.py --duration 30 --timestep 1e-2 --num_sub_steps 4 --n_solver_iters 100 --residual_threshold 1e-2 --fast --headless
+
+# Network debug
+python3.7 salamander.py --duration 10 --timestep 1e-2 --num_sub_steps 4 --n_solver_iters 100 --residual_threshold 1e-8 --fast
