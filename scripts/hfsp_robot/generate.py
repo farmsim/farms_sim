bcc1be26b8d178ffb63bf9a3e85276be3262b31a
Jonathan Arreguit <jarreguit@gmail.com>
2021-02-23 18:48:26 +0100

    [HFSP] Updated model generation

--- scripts/hfsp_robot/generate.py
+++ scripts/hfsp_robot/generate.py
@@ -3,9 +3,9 @@
 import os
 from farms_sdf.sdf import (
     ModelSDF,
-    Mesh,
-    Visual,
-    Inertial,
+    # Mesh,
+    # Visual,
+    # Inertial,
 )
 from farms_models.utils import (
     get_model_path,
@@ -17,16 +17,34 @@ def main():
     """Main"""
     # Load URDF
     model_path = get_model_path(name='hfsp_robot', version='0')
-    model = ModelSDF.from_urdf(os.path.join(
+    urdf_parth = os.path.join(
         model_path,
         'urdf',
-        'SalamanderRobot.urdf',
-    ))
+        'V2SalamanderRobot.urdf'
+        # 'SalamanderRobot.urdf',
+    )
+    model = ModelSDF.from_urdf(urdf_parth)
+    directory = os.path.dirname(urdf_parth)
 
     # Links
     print('Links:')
     for link in model.links:
         print("'{}',".format(link.name))
+        if link.visuals:
+            assert len(link.visuals) == 1
+            visual = link.visuals[0]
+            inertial = link.inertial
+            mesh_path = os.path.join(directory, visual.geometry.uri)
+            link.inertial = inertial.from_mesh(
+                path=mesh_path,
+                scale=visual.geometry.scale[0],
+                pose=visual.pose,
+                units=inertial.units,
+                mass=inertial.mass,
+            )
+            # link.inertial.inertias = inertial.inertias
+        else:
+            link.inertial.inertias = [0, 0, 0, 0, 0, 0]
 
     # Joints
     print('\nJoints:')
