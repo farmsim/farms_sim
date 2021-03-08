62e22514ffac776ab3da8bf4e1a8cb9c4683c771
Jonathan Arreguit <jarreguit@gmail.com>
2021-03-08 15:03:06 +0100

    [GENERATION] Added salamander with shorter legs

--- scripts/salamander/gen.py
+++ scripts/salamander/gen.py
@@ -9,6 +9,15 @@ def main():
         model_name='salamander',
         model_version='v3',
         n_joints_body=11,
+        leg_length=0.04,
+    generate_amphibious(
+        model_name='salamander',
+        model_version='v3_short_legs',
+        n_joints_body=11,
+        leg_length=0.02,
+        leg_radius=0.01,
+        legs_parents=[1, 4]
+    )
         leg_radius=0.01,
         legs_parents=[1, 4]
     )
