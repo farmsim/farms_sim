a9625916aa9c475092a08d8a490b28e86c273351
Jonathan Arreguit <jarreguit@gmail.com>
2021-04-27 15:36:44 +0200

    [SALAMANDER] Updated hindlimbs positions in generation

--- scripts/salamander/gen.py
+++ scripts/salamander/gen.py
@@ -5,13 +5,18 @@ from farms_amphibious.utils.generation import generate_amphibious
 
 def main():
     """Main"""
+    leg_offset = [
+        [0, 0.04, -0.02],
+        [-0.01, 0.03, -0.02],
+    ]
     generate_amphibious(
         model_name='salamander',
         model_version='v3',
         n_joints_body=11,
         leg_length=0.04,
         leg_radius=0.01,
-        legs_parents=[1, 4]
+        legs_parents=[1, 5],
+        leg_offset=leg_offset,
     )
     generate_amphibious(
         model_name='salamander',
@@ -19,7 +24,8 @@ def main():
         n_joints_body=11,
         leg_length=0.02,
         leg_radius=0.01,
-        legs_parents=[1, 4]
+        legs_parents=[1, 5],
+        leg_offset=leg_offset,
     )
     generate_amphibious(
         model_name='salamander',
@@ -27,7 +33,8 @@ def main():
         n_joints_body=11,
         leg_length=0.06,
         leg_radius=0.01,
-        legs_parents=[1, 4]
+        legs_parents=[1, 5],
+        leg_offset=leg_offset,
     )
 
 
