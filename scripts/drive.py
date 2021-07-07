9114f7989c0b9a678e3ad3aea7f4711bddac3e44
Jonathan Arreguit <jarreguit@gmail.com>
2021-07-07 21:23:08 +0200

    [SCRIPTS] Code refactoring in drive.py

--- scripts/drive.py
+++ scripts/drive.py
@@ -6,21 +6,25 @@ import time
 
 import numpy as np
 import matplotlib.pyplot as plt
-import farms_pylog as pylog
-from scipy.interpolate import splprep, splev
+
+from simple_pid import PID
 from scipy.spatial import KDTree
 from scipy.spatial.transform import Rotation
+from scipy.interpolate import splprep, splev
+
+import farms_pylog as pylog
 from farms_bullet.model.control import ControlType
 from farms_amphibious.experiment.simulation import (
     setup_from_clargs,
     simulation_setup,
     postprocessing_from_clargs,
 )
+
 from Vector_fields2 import (
     orientation_to_reach,
     theoritic_traj,
 )
-from simple_pid import PID
+
 
 def main():
     """Main"""
@@ -28,6 +32,8 @@ def main():
     # Setup simulation
     pylog.info('Creating simulation')
     clargs, sdf, animat_options, simulation_options, arena = setup_from_clargs()
+
+    # Setup
     joints = {
         joint['joint']: joint
         for joint in animat_options['control']['joints']
@@ -44,6 +50,8 @@ def main():
             muscles[name]['beta'] = 0
             muscles[name]['gamma'] = 0  # Spring stiffness
             muscles[name]['delta'] = 0  # Damping coeffiecient
+
+    # Simulation
     sim = simulation_setup(
         animat_sdf=sdf,
         animat_options=animat_options,
@@ -72,7 +80,8 @@ def main():
     MIXED = False
 
     # Initial position
-    traj, X, Y, U, V = theoritic_traj(clargs.position[0],clargs.position[1], MIXED)
+    traj, X, Y, U, V = theoritic_traj(
+        clargs.position[0], clargs.position[1], MIXED)
 
     # logfile init
     mean_ori_t = []
@@ -84,12 +93,13 @@ def main():
     pylog.info('Running simulation')
     n_iterations = sim.options['n_iterations']
     max_joint = 5
-    joint_orientation = np.zeros((max_joint,1)) # init storage of joint orientation
+    # init storage of joint orientation
+    joint_orientation = np.zeros((max_joint, 1))
     for iteration in sim.iterator(show_progress=sim.options.show_progress):
 
         # Get hydrodynamics forces
         hydro = np.array([
-        hydrodynamic.force(iteration=iteration, sensor_i=sensor_i)
+            hydrodynamic.force(iteration=iteration, sensor_i=sensor_i)
             for sensor_i in range(14)
         ])
 
@@ -128,9 +138,9 @@ def main():
             drives.array[min(iteration+1, n_iterations-1), 0] = 4.5
         elif arena_type in ['flat']:
             drives.array[min(iteration+1, n_iterations-1), 0] = 1.5
-        elif np.count_nonzero(hydro[0])<3:
+        elif np.count_nonzero(hydro[0]) < 3:
             drives.array[min(iteration+1, n_iterations-1), 0] = 1.5
-        else :
+        else:
             drives.array[min(iteration+1, n_iterations-1), 0] = 4.5
 
         # Set turn drive
@@ -159,7 +169,6 @@ def main():
                 )
             )
 
-
     # Terminate simulation
     pylog.info('Terminating simulation')
     sim.end()
@@ -174,23 +183,32 @@ def main():
     #   logfile generation
     mean_t = np.array(mean_ori_t)
     pos = np.array(pos)
-    set_t= np.array(set_t)
-    control_t= np.array(control_t)
-    x=np.arange(0,clargs.duration,clargs.timestep)
+    set_t = np.array(set_t)
+    control_t = np.array(control_t)
+    x = np.arange(0, clargs.duration, clargs.timestep)
 
     # interpolate the robot's trajectory
-    tck, u = splprep([pos[:,0], pos[:,1]], s=0)
+    tck, u = splprep([pos[:, 0], pos[:, 1]], s=0)
     pos1 = splev(u, tck)
     pos1 = np.array(pos1)
 
     # interpolate the robot's theoritic trajectory
-    tck1, u1 = splprep([traj[:,0], traj[:,1]], s=0)
+    tck1, u1 = splprep([traj[:, 0], traj[:, 1]], s=0)
     traj1 = splev(u1, tck1)
     traj1 = np.array(traj1)
     _, _, score = assess_traj(pos1, traj1)
-    print('Trajectory error is:', score)
+    pylog.info('Trajectory error is: {}'.format(score))
 
-    figs = plotting(x, pos1, control_t, mean_t, set_t, traj1, X, Y, U, V, MIXED)
+    # Plotting
+    figs = plotting(
+        t=x,
+        pos=pos1,
+        control=control_t,
+        phi=mean_t, phi_c=set_t,
+        traj=traj1,
+        X=X, Y=Y, U=U, V=V,
+        MIXED=MIXED,
+    )
 
     if clargs.save:
         for fig_i, fig in enumerate(figs):
@@ -198,16 +216,20 @@ def main():
     if not clargs.headless:
         plt.show()
 
-def plotting(t, pos, control, phi, phi_c, traj, X, Y, U, V, MIXED):
 
-    fig, ax = plt.subplots()  # Gain plot
+def plotting(t, pos, control, phi, phi_c, traj, X, Y, U, V, MIXED):
+    """Plotting"""
+
+    # Gain plot
+    fig, ax = plt.subplots()
     ax.plot(t, control, label='control output')
     ax.set_xlabel('time [s]')
     ax.set_ylabel('drive')
     ax.set_title("PID output")
     ax.legend()
 
-    fig2, ax2 = plt.subplots()  # Orientation plot
+    # Orientation plot
+    fig2, ax2 = plt.subplots()
     ax2.plot(t, np.degrees(phi_c), label='Orientation setpoint')
     ax2.plot(t, np.degrees(phi), label='mean orientation')
     plt.grid(axis='x', color='0.95')
@@ -216,15 +238,21 @@ def plotting(t, pos, control, phi, phi_c, traj, X, Y, U, V, MIXED):
     ax2.set_title("Orientation")
     ax2.legend()
 
-    fig3, ax3 = plt.subplots()  # Orientation plot
+    # Orientation plot
+    fig3, ax3 = plt.subplots()
     plt.quiver(X, Y, U, V, angles="xy")
-    ax3.plot(pos[0,:], pos[1,:], label='robot position')
-    ax3.plot(pos[0,0], pos[1,0], 'x', label='Pleurobot initial position')
-    ax3.plot(traj[0,:], traj[1,:], label='theoritic trajectory')
+    ax3.plot(pos[0, :], pos[1, :], label='robot position')
+    ax3.plot(pos[0, 0], pos[1, 0], 'x', label='Pleurobot initial position')
+    ax3.plot(traj[0, :], traj[1, :], label='theoritic trajectory')
     if MIXED:
-        circle1 = plt.Circle([3.8,0], 0.3, color='r', fill=False, label='obstacle')
+        circle1 = plt.Circle(
+            [3.8, 0],
+            0.3,
+            color='r',
+            fill=False,
+            label='obstacle',
+        )
         ax3.add_patch(circle1)
-
 
     ax3.set_xlabel('x coordinate [m]')
     ax3.set_ylabel('y coordinate [m]')
@@ -237,6 +265,7 @@ def plotting(t, pos, control, phi, phi_c, traj, X, Y, U, V, MIXED):
 
 
 def assess_traj(traj, t_traj):
+    """Assess trajectory"""
 
     tree = KDTree(np.transpose(t_traj))
     dd, ii = tree.query(np.transpose(traj), k=1)
