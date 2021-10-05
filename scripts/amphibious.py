59f0a0318c39348da5e71ac677d747ce7aae03b3
Jonathan Arreguit <jarreguit@gmail.com>
2021-10-05 13:54:48 +0200

    [SCRIPT] Refactoring

--- scripts/amphibious.py
+++ scripts/amphibious.py
@@ -6,6 +6,7 @@ import time
 import farms_pylog as pylog
 from farms_bullet.utils.profile import profile
 from farms_amphibious.utils.parse_args import parse_args
+from farms_amphibious.simulation.simulation import AmphibiousSimulation
 from farms_amphibious.experiment.simulation import (
     setup_from_clargs,
     simulation,
@@ -20,7 +21,7 @@ def main():
     clargs, sdf, animat_options, simulation_options, arena = setup_from_clargs()
 
     # Simulation
-    sim = simulation(
+    sim: AmphibiousSimulation = simulation(
         animat_sdf=sdf,
         animat_options=animat_options,
         simulation_options=simulation_options,
@@ -39,11 +40,11 @@ def main():
 
 def profile_simulation():
     """Profile simulation"""
+    tic = time.time()
     clargs = parse_args()
     profile(function=main, profile_filename=clargs.profile)
+    pylog.info('Total simulation time: {} [s]'.format(time.time() - tic))
 
 
 if __name__ == '__main__':
-    TIC = time.time()
     profile_simulation()
-    pylog.info('Total simulation time: {} [s]'.format(time.time() - TIC))
