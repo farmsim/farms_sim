01480d30c385b2b686fb2cee81346a3b2e9a92fb
Jonathan Arreguit <jarreguit@gmail.com>
2022-09-09 10:43:56 +0200

    [UTILS] Removed strtobool import (to be deprecated in Python 3.12)

--- farms_sim/utils/prompt.py
+++ farms_sim/utils/prompt.py
@@ -1,13 +1,26 @@
 """Prompt"""
 
 import os
-from distutils.util import strtobool
 
 import matplotlib.pyplot as plt
 
 from farms_core import pylog
 from farms_core.simulation.options import Simulator
 from farms_core.model.data import AnimatData
+
+
+def strtobool (val):
+    """Convert a string representation of truth to true (1) or false (0).
+    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
+    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
+    'val' is anything else.
+    """
+    val = val.lower()
+    if val in ('y', 'yes', 't', 'true', 'on', '1'):
+        return 1
+    if val in ('n', 'no', 'f', 'false', 'off', '0'):
+        return 0
+    raise ValueError(f'invalid truth value {val}')
 
 
 def prompt(query, default):
